a6a2caf0cf94203c86ed5934719c7f8a6e5644fa
Jimmy <teillard.jimmy@outlook.fr>
2021-01-11 00:10:55 +0100

    updated documentation

--- include/compressor.h
+++ include/compressor.h
@@ -5,7 +5,7 @@
 #define __COMPRESSOR_H
 
 /**
- * Calculates the color distance between two QuadTreeRGBA nodes
+ * @brief Calculates the color distance between two QuadTreeRGBA nodes
  * 
  * @param node1 The first node to compare
  * @param node2 The second node to compare
@@ -15,7 +15,7 @@
 int distRGBA(QuadTreeRGBA node1, QuadTreeRGBA node2);
 
 /**
- * Calculates the color distance between two QuadTreeBin nodes
+ * @brief Calculates the color distance between two QuadTreeBin nodes
  * 
  * @param node1 The first node to compare
  * @param node2 The second node to compare
@@ -25,7 +25,7 @@ int distRGBA(QuadTreeRGBA node1, QuadTreeRGBA node2);
 int distBin(QuadTreeBin node1, QuadTreeBin node2);
 
 /**
- * Calculates an average of given QuadTreeRGBA nodes
+ * @brief Calculates an average of given QuadTreeRGBA nodes
  * 
  * @param nodes an array of QuadTreeRGBA nodes to average
  * @param size the number of nodes
@@ -35,7 +35,7 @@ int distBin(QuadTreeBin node1, QuadTreeBin node2);
 QuadTreeRGBA avgRGBA(QuadTreeRGBA nodes[], int size);
 
 /**
- * Calculates an average of given QuadTreeBin nodes
+ * @brief Calculates an average of given QuadTreeBin nodes
  * 
  * @param nodes an array of QuadTreeBin nodes to average
  * @param size the number of nodes
@@ -45,7 +45,7 @@ QuadTreeRGBA avgRGBA(QuadTreeRGBA nodes[], int size);
 QuadTreeBin avgBin(QuadTreeBin nodes[], int size);
 
 /**
- * Calculates the general distance between two whole QuadTreeRGBA trees
+ * @brief Calculates the general distance between two whole QuadTreeRGBA trees
  * 
  * @param tree1 the first tree to compare
  * @param tree2 the second tree to compare
@@ -55,7 +55,7 @@ QuadTreeBin avgBin(QuadTreeBin nodes[], int size);
 float distTreeRGBA(QuadTreeRGBA tree1, QuadTreeRGBA tree2);
 
 /**
- * Calculates the general distance between two whole QuadTreeBin trees
+ * @brief Calculates the general distance between two whole QuadTreeBin trees
  * 
  * @param tree1 the first tree to compare
  * @param tree2 the second tree to compare
@@ -65,21 +65,21 @@ float distTreeRGBA(QuadTreeRGBA tree1, QuadTreeRGBA tree2);
 float distTreeBin(QuadTreeBin tree1, QuadTreeBin tree2);
 
 /**
- * Minimises a QuadTreeRGBA without loss
+ * @brief Minimises a QuadTreeRGBA without loss
  * 
  * @param tree the tree to be minimised
  */
 void minTreeRGBANoLoss(QuadTreeRGBA *tree);
 
 /**
- * Minimises a QuadTreeBin without loss
+ * @brief Minimises a QuadTreeBin without loss
  * 
  * @param tree the tree to be minimised
  */
 void minTreeBinNoLoss(QuadTreeBin *tree);
 
 /**
- * Minimises a QuadTreeRGBA with loss
+ * @brief Minimises a QuadTreeRGBA with loss
  * 
  * @param tree the tree to be minimised
  * @param distErr the max error distance to allow minimisation
@@ -87,7 +87,7 @@ void minTreeBinNoLoss(QuadTreeBin *tree);
 void minTreeRGBALoss(QuadTreeRGBA *tree, float distErr);
 
 /**
- * Minimises a QuadTreeBin with loss
+ * @brief Minimises a QuadTreeBin with loss
  * 
  * @param tree the tree to be minimised
  * @param distErr the max error distance to allow minimisation
