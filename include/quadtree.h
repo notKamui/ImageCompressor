3a40d6b9c109bbc0673bce2b3498b7f1191d0236
Irwin Madet <irwin.madet@gmail.com>
2021-01-10 14:54:24 +0100

    Add tree buffer system + code tweaks

--- include/quadtree.h
+++ include/quadtree.h
@@ -1,4 +1,5 @@
 /* Created by Irwin on 27/10/2020. */
+#include <stdlib.h>
 
 #ifndef __QUADTREE_H
 #define __QUADTREE_H
@@ -6,7 +7,8 @@
 #define INNER_NODE 0
 #define OUTER_NODE 1
 
-typedef struct s_quadtree_rgba {
+typedef struct s_quadtree_rgba
+{
     struct s_quadtree_rgba *northWest;
     struct s_quadtree_rgba *northEast;
     struct s_quadtree_rgba *southWest;
@@ -15,35 +17,49 @@ typedef struct s_quadtree_rgba {
     unsigned char g;
     unsigned char b;
     unsigned char a;
-} *QuadTreeRGBA;
+} * QuadTreeRGBA;
 
-typedef struct s_quadtree_bin {
+typedef struct s_quadtree_bin
+{
     struct s_quadtree_bin *northWest;
     struct s_quadtree_bin *northEast;
     struct s_quadtree_bin *southWest;
     struct s_quadtree_bin *southEast;
     unsigned char b;
-} *QuadTreeBin;
+} * QuadTreeBin;
+
+typedef struct s_quadtree_rgba_buffer
+{
+    QuadTreeRGBA *buffer;
+    size_t bufferSize;
+} * QuadTreeRGBABuffer;
+
+typedef struct s_quadtree_bin_buffer
+{
+    QuadTreeBin *buffer;
+    size_t bufferSize;
+} * QuadTreeBinBuffer;
 
 /**
- * Checks if a QuadTreeRGBA tree is actually a leaf
+ * @brief Checks if a QuadTreeRGBA tree is actually a leaf
  * 
  * @param tree the tree to be checked
  * 
  * @return true if the tree is a leaf
  */
-int RGBAIsLeaf(QuadTreeRGBA tree);
+int isLeafRGBA(QuadTreeRGBA tree);
 
 /**
- * Checks if a QuadTreeBin tree is actually a leaf
+ * @brief Checks if a QuadTreeBin tree is actually a leaf
  * 
  * @param tree the tree to be checked
  * 
  * @return true if the tree is a leaf
  */
-int BinIsLeaf(QuadTreeBin tree);
- 
-/** Allocates a QuadTreeRGBA node.
+int isLeafBin(QuadTreeBin tree);
+
+/** 
+ * @brief Allocates a QuadTreeRGBA node.
  * 
  * @param r Red value between 0 and 255.
  * @param g Green value between 0 and 255.
@@ -55,7 +71,7 @@ int BinIsLeaf(QuadTreeBin tree);
 QuadTreeRGBA allocQuadTreeRGBA(unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 
 /**
- * Allocates a QuadTreeBin node.
+ * @brief Allocates a QuadTreeBin node.
  * 
  * @param b Binary value among 0 and 1.
  * 
@@ -64,13 +80,67 @@ QuadTreeRGBA allocQuadTreeRGBA(unsigned char r, unsigned char g, unsigned char b
 QuadTreeBin allocQuadTreeBin(unsigned char b);
 
 /**
- * @brief Free memory allocated by the given QuadTreeRGBA.
+ * @brief Allocates a QuadTreeRGBA buffer structure made of an array of QuadTreeRGBA and this array's size.
+ * 
+ * @return QuadTreeRGBABuffer
  */
-void freeQuadTreeRGBA(QuadTreeRGBA tree);
+QuadTreeRGBABuffer allocQuadTreeRGBABuffer();
+
+/**
+ * @brief Allocates a QuadTreeBin buffer structure made of an array of QuadTreeBin and this array's size.
+ * 
+ * @return QuadTreeRGBABuffer
+ */
+QuadTreeBinBuffer allocQuadTreeBinBuffer();
+
+/**
+ * @brief Registers a node in the given QuadTreeRGBA buffer.
+ * 
+ * @param buffer Buffer to write.
+ * @param tree Node to register.
+ */
+void offerRGBABuffer(QuadTreeRGBABuffer *buffer, QuadTreeRGBA tree);
+
+/**
+ * @brief Registers a node in the given QuadTreeBin buffer.
+ * 
+ * @param buffer Buffer to write.
+ * @param tree Node to register.
+ */
+void offerBinBuffer(QuadTreeBinBuffer *buffer, QuadTreeBin tree);
+
+/**
+ * @brief Checks is the given RGBA node is in the given buffer.
+ * 
+ * @param buffer Buffer to search.
+ * @param tree Node to check.
+ * @return int Whether the node is buffered or not.
+ */
+int isBufferedRGBA(QuadTreeRGBABuffer buffer, QuadTreeRGBA tree);
+
+/**
+ * @brief Checks is the given bin node is in the given buffer.
+ * 
+ * @param buffer Buffer to search.
+ * @param tree Node to check.
+ * @return int Whether the node is buffered or not.
+ */
+int isBufferedBin(QuadTreeBinBuffer buffer, QuadTreeBin tree);
+
+/**
+ * @brief Free memory allocated by the given QuadTreeRGBA.
+ * 
+ * @param tree Tree to free.
+ * @param buffer Tree buffer.
+ */
+void freeQuadTreeRGBA(QuadTreeRGBA tree, QuadTreeRGBABuffer *buffer);
 
 /**
  * @brief Free memory allocated by the given QuadTreeBin.
+ * 
+ * @param tree Tree to free.
+ * @param buffer Tree buffer.
  */
-void freeQuadTreeBin(QuadTreeBin tree);
+void freeQuadTreeBin(QuadTreeBin tree, QuadTreeBinBuffer *buffer);
 
 #endif /* __QUADTREE_H */
--- src/quadtree.c
+++ src/quadtree.c
@@ -1,72 +1,179 @@
 /* Created by Irwin on 27/10/2020. */
 #include <stdlib.h>
+#include <stdio.h>
 #include "../include/quadtree.h"
 
-int RGBAIsLeaf(QuadTreeRGBA tree) {
-    return  tree->northWest == NULL && 
-            tree->southWest == NULL &&
-            tree->northEast == NULL &&
-            tree->southEast == NULL;
-}
-
-int BinIsLeaf(QuadTreeBin tree) {
-    return  tree->northWest == NULL && 
-            tree->southWest == NULL &&
-            tree->northEast == NULL &&
-            tree->southEast == NULL;
-}
-
-QuadTreeRGBA allocQuadTreeRGBA(unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
-    QuadTreeRGBA tree = malloc(sizeof(struct s_quadtree_rgba));
-
-    if (tree) {
-        tree->northWest = NULL;
-        tree->northEast = NULL;
-        tree->southWest = NULL;
-        tree->southEast = NULL;
-        tree->r = r;
-        tree->g = g;
-        tree->b = b;
-        tree->a = a;
-    }
-
-    return tree;
-}
-
-QuadTreeBin allocQuadTreeBin(unsigned char b) {
-    QuadTreeBin tree = malloc(sizeof(struct s_quadtree_bin));
-
-    if (tree) {
-        tree->northWest = NULL;
-        tree->northEast = NULL;
-        tree->southWest = NULL;
-        tree->southEast = NULL;
-        tree->b = b;
-    }
-
-    return tree;
-}
-
-void freeQuadTreeRGBA(QuadTreeRGBA tree)
+int isLeafRGBA(QuadTreeRGBA tree)
 {
-    if (tree != NULL)
+    return tree->northWest == NULL &&
+           tree->southWest == NULL &&
+           tree->northEast == NULL &&
+           tree->southEast == NULL;
+}
+
+int isLeafBin(QuadTreeBin tree)
+{
+    return tree->northWest == NULL &&
+           tree->southWest == NULL &&
+           tree->northEast == NULL &&
+           tree->southEast == NULL;
+}
+
+QuadTreeRGBA allocQuadTreeRGBA(unsigned char r, unsigned char g, unsigned char b, unsigned char a)
+{
+    QuadTreeRGBA tree;
+    if ((tree = malloc(sizeof(struct s_quadtree_rgba))) == NULL)
     {
-        freeQuadTreeRGBA(tree->northWest);
-        freeQuadTreeRGBA(tree->northEast);
-        freeQuadTreeRGBA(tree->southEast);
-        freeQuadTreeRGBA(tree->southWest);
+        fprintf(stderr, "FATAL: Allocation error.\n");
+        exit(1);
+    }
+
+    tree->northWest = NULL;
+    tree->northEast = NULL;
+    tree->southWest = NULL;
+    tree->southEast = NULL;
+    tree->r = r;
+    tree->g = g;
+    tree->b = b;
+    tree->a = a;
+
+    return tree;
+}
+
+QuadTreeBin allocQuadTreeBin(unsigned char b)
+{
+    QuadTreeBin tree;
+    if ((tree = malloc(sizeof(struct s_quadtree_bin))) == NULL)
+    {
+        fprintf(stderr, "FATAL: Allocation error.\n");
+        exit(1);
+    }
+
+    tree->northWest = NULL;
+    tree->northEast = NULL;
+    tree->southWest = NULL;
+    tree->southEast = NULL;
+    tree->b = b;
+
+    return tree;
+}
+
+QuadTreeRGBABuffer allocQuadTreeRGBABuffer()
+{
+    QuadTreeRGBABuffer buffer;
+    if ((buffer = malloc(sizeof(struct s_quadtree_rgba_buffer))) == NULL)
+    {
+        fprintf(stderr, "FATAL: Allocation error.\n");
+        exit(1);
+    }
+
+    buffer->buffer = NULL;
+    buffer->bufferSize = 0;
+    return buffer;
+}
+
+QuadTreeBinBuffer allocQuadTreeBinBuffer()
+{
+    QuadTreeBinBuffer buffer;
+    if ((buffer = malloc(sizeof(struct s_quadtree_bin_buffer))) == NULL)
+    {
+        fprintf(stderr, "FATAL: Allocation error.\n");
+        exit(1);
+    }
+
+    buffer->buffer = NULL;
+    buffer->bufferSize = 0;
+    return buffer;
+}
+
+void offerRGBABuffer(QuadTreeRGBABuffer *buffer, QuadTreeRGBA tree)
+{
+    if ((*buffer)->buffer == NULL)
+    {
+        if (((*buffer)->buffer = malloc(sizeof(QuadTreeRGBA))) == NULL)
+        {
+            fprintf(stderr, "FATAL: Allocation error.\n");
+            exit(1);
+        }
+    }
+    else
+    {
+        if (((*buffer)->buffer = realloc((*buffer)->buffer, ((*buffer)->bufferSize + 1) * sizeof(QuadTreeRGBA))) == NULL)
+        {
+            fprintf(stderr, "FATAL: Allocation error.\n");
+            exit(1);
+        }
+    }
+    (*buffer)->buffer[(*buffer)->bufferSize] = tree;
+    (*buffer)->bufferSize++;
+}
+
+void offerBinBuffer(QuadTreeBinBuffer *buffer, QuadTreeBin tree)
+{
+    if ((*buffer)->buffer == NULL)
+    {
+        if (((*buffer)->buffer = malloc(sizeof(QuadTreeBin))) == NULL)
+        {
+            fprintf(stderr, "FATAL: Allocation error.\n");
+            exit(1);
+        }
+    }
+    else
+    {
+        if (((*buffer)->buffer = realloc((*buffer)->buffer, ((*buffer)->bufferSize + 1) * sizeof(QuadTreeBin))) == NULL)
+        {
+            fprintf(stderr, "FATAL: Allocation error.\n");
+            exit(1);
+        }
+    }
+    (*buffer)->buffer[(*buffer)->bufferSize] = tree;
+    (*buffer)->bufferSize++;
+}
+
+int isBufferedRGBA(QuadTreeRGBABuffer buffer, QuadTreeRGBA tree)
+{
+    int i;
+    for (i = 0; i < buffer->bufferSize; i++)
+    {
+        if (buffer->buffer[i] == tree)
+            return 1;
+    }
+    return 0;
+}
+
+int isBufferedBin(QuadTreeBinBuffer buffer, QuadTreeBin tree)
+{
+    int i;
+    for (i = 0; i < buffer->bufferSize; i++)
+    {
+        if (buffer->buffer[i] == tree)
+            return 1;
+    }
+    return 0;
+}
+
+void freeQuadTreeRGBA(QuadTreeRGBA tree, QuadTreeRGBABuffer *buffer)
+{
+    if (!isBufferedRGBA(*buffer, tree))
+    {
+        freeQuadTreeRGBA(tree->northWest, buffer);
+        freeQuadTreeRGBA(tree->northEast, buffer);
+        freeQuadTreeRGBA(tree->southEast, buffer);
+        freeQuadTreeRGBA(tree->southWest, buffer);
+        offerRGBABuffer(buffer, tree);
         free(tree);
     }
 }
 
-void freeQuadTreeBin(QuadTreeBin tree)
+void freeQuadTreeBin(QuadTreeBin tree, QuadTreeBinBuffer *buffer)
 {
-    if (tree != NULL)
+    if (tree != NULL && !isBufferedBin(*buffer, tree))
     {
-        freeQuadTreeBin(tree->northWest);
-        freeQuadTreeBin(tree->northEast);
-        freeQuadTreeBin(tree->southEast);
-        freeQuadTreeBin(tree->southWest);
+        freeQuadTreeBin(tree->northWest, buffer);
+        freeQuadTreeBin(tree->northEast, buffer);
+        freeQuadTreeBin(tree->southEast, buffer);
+        freeQuadTreeBin(tree->southWest, buffer);
+        offerBinBuffer(buffer, tree);
         free(tree);
     }
 }
